a141ea4f2107f70d6e119d02f2229c71bfbcdbcf
levvw <abdelraahmanmostafa@gmail.com>
2021-09-27 17:36:46 +0200

    responsive styled navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,12 +1,28 @@
-import classes from "./Nav.module.css";
+import "./Nav.css";
+import { useState } from "react";
 function Nav() {
+  const [navClass, setNavClass] = useState("navbar-links");
+  function onClickHandler() {
+    if (navClass === "navbar-links") setNavClass("navbar-links active");
+    if (navClass === "navbar-links active") setNavClass("navbar-links");
+  }
   return (
-    <ul className={classes.list}>
-      <li>About</li>
-      <li>Work</li>
-      <li>Skills</li>
-      <li>Contact</li>
-    </ul>
+    <nav class="navbar">
+      <div class="brand-title">Levw</div>
+      <a href="#" class="toggle-button" onClick={onClickHandler}>
+        <span class="bar"></span>
+        <span class="bar"></span>
+        <span class="bar"></span>
+      </a>
+      <div class={navClass}>
+        <ul>
+          <li>About</li>
+          <li>Work</li>
+          <li>Skills</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+    </nav>
   );
 }
 export default Nav;
