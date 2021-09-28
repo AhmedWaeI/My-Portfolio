3d4a37663fc6030d6908a43562521e1b93f72e88
levvw <abdelraahmanmostafa@gmail.com>
2021-09-29 00:55:12 +0200

    added socials github and linkedin

--- src/Footer.js
+++ src/Footer.js
@@ -1,9 +1,30 @@
+import React from "react";
 import classes from "./Footer.module.css";
 function Footer() {
   return (
-    <div className={classes.footer}>
-      Desgined &#38; Built by Abdelrahman Mostafa
-    </div>
+    <React.Fragment>
+      <div className={classes.links}>
+        <a target="_blank" href="https://github.com/Levvw" rel="noreferrer">
+          <img
+            alt="github"
+            src="https://img.icons8.com/ios-filled/50/000000/github.png"
+          />
+        </a>
+        <a
+          target="_blank"
+          href="https://www.linkedin.com/in/levw/"
+          rel="noreferrer"
+        >
+          <img
+            alt="linkedin"
+            src="https://img.icons8.com/ios-filled/50/000000/linkedin-circled--v2.png"
+          />
+        </a>
+      </div>
+      <div className={classes.footer}>
+        Desgined &#38; Built by Abdelrahman Mostafa
+      </div>
+    </React.Fragment>
   );
 }
 export default Footer;
