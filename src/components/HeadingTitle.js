bdda0ec39fa81a6ac3ba0c93c843176575b978ce
levvw <abdelraahmanmostafa@gmail.com>
2023-02-19 19:51:20 +0200

    changed header title component

--- src/components/HeadingTitle.js
+++ src/components/HeadingTitle.js
@@ -1,9 +1,11 @@
 import classes from "./HeadingTitle.module.css";
 function HeadingTitle(props) {
   return (
-    <div className={classes.seven}>
-      <h1>{props.title}</h1>
+    <div className={classes.title}>
+      <div className={classes.outerTitle}>{props.title.toUpperCase()}</div>
+      <div className={classes.innerTitle}>{props.title}</div>
     </div>
   );
 }
+
 export default HeadingTitle;
